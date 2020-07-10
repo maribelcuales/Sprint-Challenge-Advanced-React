bf4316c3fc4138a791cc7abfbb819f88f56ad70a
maribelcuales <maribelcuales@gmail.com>
2020-07-10 19:06:50 +0800

    Added test for checking if header title is visible.

---
+++ client/src/components/Players.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect'
+import Players from './Players'; 
+import Navbar from './Navbar'; 
+
+
+test("The Players header is visible", () => {
+  const { getByText } = render(<Navbar />);
+
+  expect(getByText(/the players/i)).toBeVisible();  
+}); 
+
